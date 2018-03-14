58726df886d5aec9ccdddcb0fcc50441f9c2bab0
Unknown <Zachary.Lynch1337@gmail.com>
2018-03-14 12:18:11 -0800

    Added basic code for onClickListener

--- app/src/main/java/funfacts/com/example/android/funfacts/MainActivity.java
+++ app/src/main/java/funfacts/com/example/android/funfacts/MainActivity.java
@@ -2,12 +2,29 @@ package funfacts.com.example.android.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare View variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign the views from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactBtn);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
