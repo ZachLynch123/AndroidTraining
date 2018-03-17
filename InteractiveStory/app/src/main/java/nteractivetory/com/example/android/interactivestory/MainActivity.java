374dafe3650668c223696511b83978735a244ecd
Unknown <Zachary.Lynch1337@gmail.com>
2018-03-17 12:00:11 -0800

    Added new activity and made a new class to handle screen orientation for all activities

--- InteractiveStory/app/src/main/java/nteractivetory/com/example/android/interactivestory/MainActivity.java
+++ InteractiveStory/app/src/main/java/nteractivetory/com/example/android/interactivestory/MainActivity.java
@@ -1,13 +1,36 @@
 package nteractivetory.com.example.android.interactivestory;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText nameField;
+    private Button startButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = (EditText)findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameField.getText().toString();
+                StartStory();
+            }
+        });
+
+    }
+
+    private void StartStory() {
+        
     }
 }
