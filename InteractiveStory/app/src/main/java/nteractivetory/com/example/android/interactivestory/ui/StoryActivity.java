f09881783713818e20fbd58f8abaca235f5419f0
Unknown <Zachary.Lynch1337@gmail.com>
2018-03-19 14:50:34 -0800

    Elimintated hard coding of button styles by adding a new style.

--- InteractiveStory/app/src/main/java/nteractivetory/com/example/android/interactivestory/ui/StoryActivity.java
+++ InteractiveStory/app/src/main/java/nteractivetory/com/example/android/interactivestory/ui/StoryActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import nteractivetory.com.example.android.interactivestory.R;
 import nteractivetory.com.example.android.interactivestory.model.Page;
+import nteractivetory.com.example.android.interactivestory.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
 
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,11 @@ public class StoryActivity extends AppCompatActivity {
         }
         Log.d(TAG, name);
 
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        Page page = story.getPage(pageNumber);
     }
 }
