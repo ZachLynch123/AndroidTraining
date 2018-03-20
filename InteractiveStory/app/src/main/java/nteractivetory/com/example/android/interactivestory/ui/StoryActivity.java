3bfdc1540acb1827dabcc4ab7a04dd9944a94d9e
Unknown <Zachary.Lynch1337@gmail.com>
2018-03-19 16:04:25 -0800

    App crashes at the end of story due to null pointer exception
    Need to have at least one button on the last page to eliminate the null pointer exception by using the isFinalPage method in the page class. Refactored the button functionallity into it's own method

--- InteractiveStory/app/src/main/java/nteractivetory/com/example/android/interactivestory/ui/StoryActivity.java
+++ InteractiveStory/app/src/main/java/nteractivetory/com/example/android/interactivestory/ui/StoryActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -47,7 +48,7 @@ public class StoryActivity extends AppCompatActivity {
     }
 
     private void loadPage(int pageNumber) {
-        Page page = story.getPage(pageNumber);
+        final Page page = story.getPage(pageNumber);
         Drawable image = ContextCompat.getDrawable(this, page.getImageId());
         storyImageView.setImageDrawable(image);
 
@@ -55,8 +56,35 @@ public class StoryActivity extends AppCompatActivity {
         // Add name if placeholder included. Won't add if not
         pageText = String.format(pageText, name);
         storyTextView.setText(pageText);
-        choice1Button.setText(page.getChoice1().getTextId());
-        choice2Button.setText(page.getChoice2().getTextId());
 
+        if (page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+
+        } else {
+
+
+            loadButtons(page);
+
+    }
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
     }
 }
